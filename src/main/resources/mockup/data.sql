eb67c65c4ad08a93d61df5788a8a4f4a9d940cc1
Miroslav Milinkov <miroslav.milinkov@persida-bio.com>
2023-11-06 03:06:17 +0100

    added valid final call and inheritance  values

--- src/main/resources/mockup/data.sql
+++ src/main/resources/mockup/data.sql
@@ -3,10 +3,10 @@ INSERT INTO `pc_local`.`pc_user` (`user_id`, `created_by`, `created_on`, `modifi
 INSERT INTO `pc_local`.`pc_user` (`user_id`, `created_by`, `created_on`, `modified_by`, `modified_on`, `email`, `enabled`, `password`, `role`, `username`, `first_name`, `last_name`)VALUES (2, 'PC_BACKEND' ,NOW() ,'PC_BACKEND' ,NOW(), 'bosko.jeftic@persida-bio.com', 1, '$2a$10$kCIA24uD1XdQd4/T.E6dsOJK9usnpHnNCv//ZW7WNkkuzaoNTU2Qq','USER', 'pcbosko', 'bosko', 'jeftic');
 
 --insert into final_call table
-INSERT INTO `pc_local`.`final_call` (`term`) VALUE ('INSUFFICIENT'),('LIKELY_BENIGN'),('BENIGN'),('UNCERTAIN'),('LIKELY_PATHOGENIC'),('PATHOGENIC'),('CONFLICTING');
+INSERT INTO `pc_local`.`final_call` (`term`) VALUE ('Uncertain Significance - Insufficient Evidence'),('Likely Benign'),('Benign'),('Uncertain Significance'),('Likely Pathogenic'),('Pathogenic'),('Uncertain Significance - Conflicting Evidence');
 
 --insert into inheritance table
-INSERT INTO `pc_local`.`inheritance` (`term`) VALUE ('AUTOSOMAL_DOMINANT'),('AUTOSOMAL_RECESSIVE'),('X_LINKED_DOMINANT'),('X_LINKED_RECESSIVE'),('MITOCHONDRIAL'),('MULTIFACTORIAL'),('OTHER'),('UNKNOWN');
+INSERT INTO `pc_local`.`inheritance` (`term`) VALUE ('Autosomal Dominant'),('Autosomal Recessive'),('X-linked Dominant'),('X-linked Recessive'),('Mitochondrial'),('Multifactoral'),('Other'),('Unknown');
 
 --insert into variant table
 INSERT INTO `pc_local`.`variant` (`created_by`, `created_on`, `modified_by`, `modified_on`, `caid`) VALUES ('PC_BACKEND', NOW(), 'PC_BACKEND', NOW(), 'CA321211');
