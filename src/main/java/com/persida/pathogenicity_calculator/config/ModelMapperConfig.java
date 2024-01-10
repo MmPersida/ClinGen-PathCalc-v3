265450c1ceb64e6eb9504786f7655b801eac15c8
Miroslav Milinkov <miroslav.milinkov@persida-bio.com>
2024-01-10 01:28:54 +0100

    model mapper issue

---
+++ src/main/java/com/persida/pathogenicity_calculator/config/ModelMapperConfig.java
@@ -0,0 +1,11 @@
+package com.persida.pathogenicity_calculator.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
